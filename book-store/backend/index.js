a641ee998c23ec938ee0bab96eeb3699b0995d6c
tchar <trishitchar@gmail.com>
2024-03-17 01:36:38 +0530

    almost backend complete

--- book-store/backend/index.js
+++ book-store/backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT,MONGODB_URL } from "./config.js";
 import mongoose from 'mongoose'
 import cors from 'cors'
@@ -30,17 +30,74 @@ app.post('/books',async(request,response)=>{
 
         return response.status(201).send(book)
         
-    }catch(e){
-        console.log(e.message);
-        return response.status(500).send({message:e.message})
+    }catch (error) {
+        console.log(error.message);
+        return response.status(500).send({message:error.message})
     }
 
 })
 
-app.get('/books',(request,response)=>{
-
+// get all books from db
+app.get('/books',async(request,response)=>{
+    try {
+        const books = await Book.find({})
+        return response.status(200).json({
+            count: books.length,
+            data:books
+        })
+    } catch (error) {
+        console.log(error.message);
+        return response.status(500).send({message:error.message})
+    }
 })
 
+//get one particular book
+app.get('/books/:id',async(request,response)=>{
+    try {
+        const {id}= request.params;
+        const book = await Book.findById(id);
+        return response.status(200).json(book)
+    } catch (error) {
+        console.log(error.message);
+        return response.status(500).send({message:error.message})
+    }
+})
+
+//to update
+app.put('/books/:id',async (request,response)=>{
+    try {
+        if(!request.body.title || !request.body.author || !request.body.publishYear){
+            return response.status(400).send({
+                message:'send all field'
+            })
+        }
+        const {id} = request.params;
+        const result = await Book.findByIdAndUpdate(id,request.body)
+        if(!result){
+            return response.status(500).send({message:'book not found'})
+        }
+        return response.status(200).send({message: 'book updated successfully'})
+    } catch (error) {
+        console.log(error.message);
+        return response.status(500).send({message:error.message})
+    }
+})
+
+//delte somthing
+app.delete('/books/:id',async(request,response)=>{
+    try {
+        const {id}= request.params;
+        const result = await Book.findByIdAndDelete(id);
+        if(!result){
+            return response.status(500).send({message:'book not found'})
+        }
+        return response.status(200).send({message: 'book deleted successfully'})
+    } catch (error) {
+        console.log(error.message);
+        return response.status(500).send({message:error.message})
+    }
+
+})
 
 mongoose.connect(MONGODB_URL)
     .then(()=>{
