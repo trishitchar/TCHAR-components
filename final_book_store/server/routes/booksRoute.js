55e7c118c7767b3a32b764c3ba29df3ba9fbda1c
tchar <trishitchar@gmail.com>
2024-03-25 11:04:30 +0530

    some backend stuff

--- final_book_store/server/routes/booksRoute.js
+++ final_book_store/server/routes/booksRoute.js
@@ -26,11 +26,76 @@ router.post('/upload-book',async(req,res)=>{
 router.get('/all-books',async(req,res)=>{
     try {
         const result = await Book.find({});
-        res.send(200).json(result)
+        res.status(200).json({result})
     } catch (error) {
         console.log(error);
         res.status(500).json({message: 'Server error'})
     }
 })
+
+//get any specific book with id
+router.get('/:id',async(req,res)=>{
+    try {
+        const id = req.params.id;
+        const result =await Book.findById(id);
+        if (!result) {
+            return res.status(404).json({ message: 'Book not found' }); 
+        }
+        res.status(200).json(result)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: 'Server error'})
+    }
+})
+
+router.get('/:category', async (req, res) => {
+    try {
+      const category = req.params.category;
+      const result = await Book.find({ category});
+      if (result.length === 0) {
+        return res.status(404).json({ message: `No books found in category: ${category}` });
+      }
+      res.status(200).json({ message: `Books in category '${category}' retrieved successfully`, data: result });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
+
+
+
+router.put('/:id',async(req,res)=>{
+    try {
+        const id = req.params.id;
+        const { authorName, imageUrl, category, description, title, bookPdfUrl } = req.body;
+
+        if (!authorName || !imageUrl || !category || !description || !title) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+        const result = await Book.findByIdAndUpdate(id,req.body);
+        if(!result){
+            return res.status(500).send({message:'book not found'})
+        }
+        return res.status(200).send({message: 'book updated successfully', data:req.body})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: 'Server error'})
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const result = await Book.findByIdAndDelete(id);
+        if (!result) {
+            return res.status(404).json({ message: 'Book not found' });
+        }
+        return res.status(200).json({ message: 'Book deleted successfully', data: result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
 
 export default router;
